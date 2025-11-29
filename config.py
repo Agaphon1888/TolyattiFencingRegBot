6211dddf9dc39d5fb4e88f6f48ea1934aebff33a
Agaphon1888 <egorov2000@inbox.ru>
2025-11-29 15:22:44 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', 'YOUR_BOT_TOKEN')
+    WEBHOOK_URL = os.environ.get('WEBHOOK_URL', '') + '/webhook'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-here')
+    
+    # Настройки формы регистрации
+    WEAPON_TYPES = ['Сабля', 'Шпага', 'Рапира']
+    CATEGORIES = ['Юниоры', 'Взрослые', 'Ветераны']
+    AGE_GROUPS = ['до 12 лет', '13-15 лет', '16-18 лет', '19+ лет']
